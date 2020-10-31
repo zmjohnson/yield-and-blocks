b5f12d67b3068d6b58f6abcae666c651c93927e5
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-10-30 22:56:00 -0400

    commit tree 132b65f8a9e0acb530c1734b856577587ab36abb

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
-end
-
-# call your method here!
+def hello_t(array)
+    if block_given?
+      i = 0
+   
+      while i < array.length
+        yield(array[i])
+        i = i + 1
+      end
+   
+      array
+    else
+      puts "Hey! No block was given!"
+    end
+  end
+  
+  hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
+  end
 
